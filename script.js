2ae4aa25513e22dc5c5ec7de396671f38b1f23bc
AName0 <115248908+AName0@users.noreply.github.com>
2023-08-02 18:42:45 +0100

    can now draw by holding down mouse and dragging

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const GRID_SPACE = 800;
 const grid = document.getElementById("grid");
+const body = document.querySelector("body");
+let mouseIsDown;
 
 function createGrid(numOfRows) {
     for (let i=0; i < numOfRows**2; i++) {
@@ -8,8 +10,23 @@ function createGrid(numOfRows) {
         square.style.width = `${GRID_SPACE / numOfRows}px`;
         square.style.height = `${GRID_SPACE / numOfRows}px`;
         square.style.outline = "1px solid black";
+        square.addEventListener("mouseover", colorSquares);
         grid.appendChild(square);
     }
 }
+
+function colorSquares(e) {
+    if (mouseIsDown == true) {
+        e.target.style.backgroundColor = "black";
+    }
+}
+
+body.addEventListener("mousedown", function (e) {
+    mouseIsDown = true;
+})
+
+body.addEventListener("mouseup", function (e) {
+    mouseIsDown = false;
+})
 
 createGrid(16);
