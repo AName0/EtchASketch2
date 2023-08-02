d21a94c93d3d66064639acb917aae4bcf5c65351
AName0 <115248908+AName0@users.noreply.github.com>
2023-08-02 19:46:08 +0100

    wrote code for all drawing modes including a function to generate random colors

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const GRID_SPACE = 800;
 const grid = document.getElementById("grid");
 const body = document.querySelector("body");
+const normalModeButton = document.querySelector(".normal");
+const randomModeButton = document.querySelector(".random");
+const eraserButton = document.querySelector(".eraser");
+let colorPicker = document.getElementById("pencil-color");
 let gridSizeText = document.getElementById("grid-size-text");
 let gridSizeSlider = document.getElementById("grid-size-slider");
 let mouseIsDown;
+let pencilColor = "rgba(0, 0, 0)"
+let isNormalMode = true;
+let isRandomMode = false;
+let isEraserMode = false;;
 
 function createGrid(numOfRows) {
     for (let i=0; i < numOfRows**2; i++) {
@@ -17,9 +25,23 @@ function createGrid(numOfRows) {
     }
 }
 
+function generateRandomColor() {
+    let RGB1 = Math.floor(Math.random() * 256);
+    let RGB2 = Math.floor(Math.random() * 256);
+    let RGB3 = Math.floor(Math.random() * 256);
+    let randomColor = "rgb(" + RGB1 + ", " + RGB2 + ", " + RGB3 + ")";
+    return randomColor;
+}
+
 function colorSquares(e) {
-    if (mouseIsDown == true) {
-        e.target.style.backgroundColor = "black";
+    if (mouseIsDown === true) {
+        if (isNormalMode === true) {
+            e.target.style.backgroundColor = pencilColor;
+        } else if (isRandomMode === true) {
+            e.target.style.backgroundColor = generateRandomColor();
+        } else if (isEraserMode === true) {
+            e.target.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        }
     }
 }
 
@@ -36,5 +58,39 @@ gridSizeSlider.oninput = function() {
     createGrid(this.value);
     gridSizeText.textContent = `Grid Size: ${this.value}`;
 }
+
+colorPicker.oninput = function() {
+    pencilColor = this.value;
+}
+
+normalModeButton.addEventListener("click", () => {
+    if(isNormalMode === true) {
+        return;
+    } else {
+        isNormalMode = true;
+        isRandomMode = false;
+        isEraserMode = false;
+    }
+})
+
+randomModeButton.addEventListener("click", () => {
+    if(isRandomMode === true) {
+        return;
+    } else {
+        isNormalMode = false;
+        isRandomMode = true;
+        isEraserMode = false;
+    }
+})
+
+eraserButton.addEventListener("click", () => {
+    if(isEraserMode === true) {
+        return;
+    } else {
+        isNormalMode = false;
+        isRandomMode = false;
+        isEraserMode = true;
+    }
+})
 
 createGrid(16);
