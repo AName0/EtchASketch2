cb8d37aed14a9e54752c6a4cf9ddc1b696d3ba0c
AName0 <115248908+AName0@users.noreply.github.com>
2023-08-02 17:56:59 +0100

    createGrid now adds squares which change size dependant on numOfRows

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-const gridContainer = document.getElementById("grid-container");
+const GRID_SPACE = 800;
+const grid = document.getElementById("grid");
 
 function createGrid(numOfRows) {
     for (let i=0; i < numOfRows**2; i++) {
         let square = document.createElement("div");
-        gridContainer.appendChild(square);
+        square.className = "square";
+        square.style.width = `${GRID_SPACE / numOfRows}px`;
+        square.style.height = `${GRID_SPACE / numOfRows}px`;
+        square.style.outline = "1px solid black";
+        grid.appendChild(square);
     }
 }
 
